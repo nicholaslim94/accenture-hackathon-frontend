230e7d23145510e00167b82bbaf38d2ba4fedd9c
Muhammad-MO <64999609+Muhammad-MO@users.noreply.github.com>
2021-07-27 23:40:37 +0800

    added images and background-color

---
+++ src/components/landingPage/landing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Landing.css';
+
+
+
+function Landing() {
+    return (
+      
+        <div className = "Main">
+            <h1> Vaccinelah </h1>    
+
+
+
+</div>
+    )
+}
+
+export default Landing;
