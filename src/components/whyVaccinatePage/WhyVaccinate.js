af257d442c4dbbd9d7f94e6142026d9f94b91b89
Muhammad-MO <64999609+Muhammad-MO@users.noreply.github.com>
2021-07-28 22:08:54 +0800

    add youtuve video carousel

--- src/components/whyVaccinatePage/WhyVaccinate.js
+++ src/components/whyVaccinatePage/WhyVaccinate.js
@@ -1,6 +1,8 @@
 import "./WhyVaccinate.css";
 import MainNavbar from "../navbar/MainNavbar";
 import Container from "react-bootstrap/Container";
+import { Carousel } from "react-bootstrap";
+import ReactPlayer from 'react-player';
 
 function WhyVaccinate() {
   return (
@@ -8,6 +10,49 @@ function WhyVaccinate() {
       <MainNavbar />
       <Container className="whyVaccinate-content">
         <h1>Here are the reasons as to why it is good to vaccinate...</h1>
+       <Carousel>
+  
+        <Carousel.Item>
+       
+        <ReactPlayer url='https://youtu.be/69JKL8jRfWw'  
+            height="500px"
+            width="1300px"
+            alt="first video"
+                   />
+          
+        </Carousel.Item>
+
+
+        <Carousel.Item>
+       
+       <ReactPlayer url='https://youtu.be/cVrzbws4jU8'  
+       height="500px"
+       width="1300px"
+       alt="2nd video"
+              />
+     
+       </Carousel.Item>
+
+
+       <Carousel.Item>
+       
+       <ReactPlayer url='https://youtu.be/wclXZ4W7fwo'  
+       height="500px"
+       width="1300px"
+       alt="third video"
+              />
+     
+       </Carousel.Item>
+
+
+
+
+
+
+
+    
+      </Carousel>
+
       </Container>
     </div>
   );
