782c9f549d150818fd85ac3763e113cc513c21e1
Muhaimin <shutup_95@hotmail.com>
2021-07-30 02:28:16 +0800

    fixed constants issue

--- src/constants/rest.js
+++ src/constants/rest.js
@@ -1,10 +1,9 @@
-export const BASE_URL = "http://localhost:8080"
-export const LOGIN = BASE_URL + "/login"
-export const REGISTER = BASE_URL + "/register"
-export const ACCOUNT_DETAILS = BASE_URL + "/account/get/details"
-export const UPDATE_DETAILS = BASE_URL + "/account/update/details"
-export const ACCOUNT_REWARDS = BASE_URL + "account/get/rewards"
-export const ACCOUNT_ADD_REWARDS = BASE_URL + "/account/add/rewards/"
+export const BASE_URL = "http://localhost:8080";
+export const LOGIN = BASE_URL + "/login";
+export const REGISTER = BASE_URL + "/register";
+export const ACCOUNT_DETAILS = BASE_URL + "/account/get/details";
+export const UPDATE_DETAILS = BASE_URL + "/account/update/details";
+export const ACCOUNT_REWARDS = BASE_URL + "/account/get/rewards";
+export const ACCOUNT_ADD_REWARDS = BASE_URL + "/account/add/rewards/";
 
-
-export const GET_REWARDS = BASE_URL + "/reward/get/0/9999"
+export const GET_REWARDS = BASE_URL + "/reward/get/0/9999";
